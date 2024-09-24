479210c65ceb98ddfd6d33f1b268d0b992a28bfb
Obaker815 <164036226+Obaker815@users.noreply.github.com>
2024-09-24 20:39:42 +0100

    Added example usage

---
+++ Merge-sort/Main.cs
@@ -0,0 +1,27 @@
+﻿using Merge_sort;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input a scentence from the user
+            Console.WriteLine("input scentence to be merge sorted");
+            string inputScentence = Console.ReadLine() + "";
+
+            // remove whitespace
+            inputScentence.Trim();
+
+            // turns the input into a character array
+            char[] scentenceArray = new char[inputScentence.Length];
+            for (int i = 0; i < scentenceArray.Length; i++)
+            {
+                scentenceArray[i] = inputScentence[i];
+            }
+
+            // writes the array to the console
+            Console.WriteLine(scentenceArray.MergeSort());
+        }
+    }
+}
